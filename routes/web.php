4f21d025a31d6bf8599be842dc70a6660f039522
Tejinder Singh <tejinder94782@gmail.com>
2023-07-24 23:55:19 +0530

    Laravel Test.

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', function () {
